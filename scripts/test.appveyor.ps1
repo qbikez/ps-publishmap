7e70e03a58c28d775c57f5ceaa5980bbe18e573d
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-02-07 07:18:20 +0100

    fix appveyor test script

--- scripts/test.appveyor.ps1
+++ scripts/test.appveyor.ps1
@@ -1,8 +1,31 @@
+function ExitWithCode 
+{ 
+    param 
+    ( 
+        $exitcode 
+    )
+
+    $host.SetShouldExit($exitcode) 
+    exit 
+}
+
+$artifacts = "$psscriptroot\..\artifacts"
+
+try {
 if (test-path "$artifacts\test-result.xml") {
     remove-item "$artifacts\test-result.xml"
 }
 
-& "$PSScriptRoot\test.ps1"
+$testResultCode = & "$PSScriptRoot\test.ps1"
+
+if (!(test-path "$artifacts\test-result.xml")) {
+    throw "test results not found at $artifacts\test-result.xml!"
+}
+
+$content = get-content "$artifacts\test-result.xml" | out-string
+if ([string]::isnullorwhitespace($content)) {
+    throw "$artifacts\test-result.xml is empty!"
+}
 
 $url = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
 #$url = https://ci.appveyor.com/api/testresults/nunit/bq558ckwevwb47qb
@@ -17,4 +40,11 @@ write-host "upload done. result = $r"
 } 
 finally {
     $wc.Dispose()
+}
+write-host "pester result = '$testResultCode' lastexitcode=$lastexitcode"
+
+#ExitWithCode $testResultCode
+
+} catch {
+    ExitWithCode 1  
 }
--- scripts/test.ps1
+++ scripts/test.ps1
@@ -4,6 +4,8 @@ import-module $pesterPath
 
 $artifacts = "$psscriptroot\..\artifacts"
 if (!(Test-Path $artifacts)) {
-    new-item $artifacts -ItemType directory
+    $null = new-item $artifacts -ItemType directory
 }
-Invoke-Pester "$psscriptroot\..\test\test.ps1" -OutputFile $artifacts\test-result.xml -OutputFormat NUnitXml
+$r = Invoke-Pester "$psscriptroot\..\test" -OutputFile "$artifacts\test-result.xml" -OutputFormat NUnitXml -EnableExit
+
+return $r
