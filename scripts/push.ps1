9da082931940f0c62de683e412cf3575439cb2b6
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-02-17 03:55:54 +0100

    update push script

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -1,27 +1,35 @@
 [CmdletBinding(SupportsShouldProcess=$true)]
 param([switch][bool]$newversion)
 
-$envscript = "$psscriptroot\..\.env.ps1" 
-if (test-path "$envscript") {
-    . $envscript
+
+function push-module {
+[CmdletBinding(SupportsShouldProcess=$true)]
+param($modulepath, [switch][bool]$newversion)
+
+    $envscript = "$psscriptroot\..\.env.ps1" 
+    if (test-path "$envscript") {
+        . $envscript
+    }
+
+    $repo = "$env:PS_PUBLISH_REPO"
+    $key = "$env:PS_PUBLISH_REPO_KEY"
+
+    . $psscriptroot\imports\set-moduleversion.ps1
+    . $psscriptroot\imports\nuspec-tools.ps1
+
+    $ver = get-moduleversion $modulepath
+    if ($newversion) {
+        $newver = Incremet-Version $ver
+    } else {
+        $newver = $ver
+    }
+    set-moduleversion $modulepath -version $newver
+
+
+    Publish-Module -Path $modulepath -Repository $repo -Verbose -NuGetApiKey $key
+
 }
 
-$repo = "$env:PS_PUBLISH_REPO"
-$key = "$env:PS_PUBLISH_REPO_KEY"
-
-. $psscriptroot\imports\set-moduleversion.ps1
-. $psscriptroot\imports\nuspec-tools.ps1
-
-$modulepath = "$psscriptroot\..\src\publishmap"
-
-$ver = get-moduleversion $modulepath
-if ($newversion) {
-    $newver = Incremet-Version $ver
-} else {
-    $newver = $ver
-}
-set-moduleversion $modulepath -version $newver
-
-
-Publish-Module -Path $modulepath -Repository $repo -Verbose -NuGetApiKey $key
-
+$root = $psscriptroot
+$modules = get-childitem "$root\..\src" -filter "*.psm1" -recurse | % { $_.Directory.FullName }
+$modules | % { push-module $_ -newversion:$newversion }
