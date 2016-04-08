6359eccf63df7b2a751eda245c2167d1305609a2
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-04-08 11:37:16 +0200

    do not use resolve-path

--- src/publishmap/imports.ps1
+++ src/publishmap/imports.ps1
@@ -1,6 +1,6 @@
 # grab functions from files
-Resolve-Path $psscriptroot\functions\*.ps1 | 
-    ? { -not ($_.ProviderPath.Contains(".Tests.")) } |
-    ? { -not ((split-path -leaf $_).StartsWith("_")) } |
-    % { . $_.ProviderPath }
+get-childitem $psscriptroot\functions\ -filter "*.ps1" | 
+    ? { -not ($_.name.Contains(".Tests.")) } |
+    ? { -not (($_.name).StartsWith("_")) } |
+    % { . $_.fullname }
 
