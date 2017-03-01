613771fb97523d9d192fd34aa86d990aa4a80be7
jakub.pawlowski <pawlowskijakub@gmail.com>
2017-03-01 07:27:06 +0100

    switch to netstandard1.6

---
+++ src/publishmap/utils/publishmap.powershell/PublishmapModule.cs
@@ -0,0 +1,13 @@
+using System.Management.Automation;
+
+namespace Publishmap.Powershell
+{
+    [Cmdlet(VerbsCommon.Add, "BinaryModuleCmdlet1")]
+    public class TestBinaryModuleCmdlet1Command : Cmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            
+        }
+    }  
+}
