7be45a90a5cc62a1c399ef59516c8ad8eec8d633
jakub.pawlowski <jakub.pawlowski@guestline.com>
2024-11-17 07:34:09 +0100

    recursive

--- src/configmap/configure.tests.ps1
+++ src/configmap/configure.tests.ps1
@@ -2,26 +2,34 @@ BeforeAll {
     . $PSScriptRoot/.config-utils.ps1
 
     function Get-CompletionList($modules) {
-        if ($modules -is [array]) {
-            return $modules
+        $result = [ordered]@{}
+
+        $l = $modules
+        if ($modules.list) {
+            $l = $modules.list
+        }
+        if ($l -is [scriptblock]) {
+            $submodules = Invoke-Command -ScriptBlock $l
+        }
+        elseif ($l -is [array]) {
+            $submodules = $l | % { $r = @{} } { $r[$_] = $_ } { $r }
+        }
+        
+        if ($submodules) {
+            foreach ($sub in $submodules.GetEnumerator()) {
+                $result[$sub.key] = $sub.value
+            }
+            return $result
         }
 
-        $result = [ordered]@{}
         foreach ($kvp in $modules.GetEnumerator()) {
             $module = $kvp.value
             if ($module.list) {
                 $groupKey = "$($kvp.key)*"
                 $result.$groupKey = $module
                 
-                if ($module.list -is [scriptblock]) {
-                    $submodules = Invoke-Command -ScriptBlock $module.list
-                } elseif ($module.list -is [array]) {
-                    $submodules = $module.list | % { $r = @{} } { $r[$_] = $_ } { $r }
-                }
-
-                foreach ($sub in $submodules.GetEnumerator()) {
-                    $result[$sub.key] = $sub.value
-                }
+                $submodules = Get-CompletionList $module
+                $result += $submodules
             }
             else {
                 $groupKey = "$($kvp.key)"
@@ -32,42 +40,36 @@ BeforeAll {
     }
 }
 
-Describe 'configuration map' {
-    Describe "simple list" {
-        BeforeAll {
-            $map = @("item1", "item2")
-        }
-        It 'resolves to list of keys' {
-            $result = Get-CompletionList $map
-            $result | Should -Be @("item1", "item2")
-        }
-    }
 
-    Describe "simple map" {
-        BeforeAll {
-            $map = [ordered]@{ 
-                "key1" = @{ id = "a" }
-                "key2" = @{ id = "b" }
-            }
-        }
-        It 'resolves to list of keys' {
-            $result = Get-CompletionList $map
-            $result.Keys | Should -be @("key1", "key2")
-        }
+Describe 'configuration map' -ForEach @(
+    @{
+        Name = "simple list"
+        Map  = @("item1", "item2")
+        Keys = @("item1", "item2")
     }
-
-    Describe "one-level simple list" {
-        BeforeAll {
-            $map = [ordered]@{ 
-                "key1" = @{ 
-                    list = ("a", "b")
-                }
-                "key2" = @{ id = "b" }
-            }
+    @{
+        Name = "simple map"
+        Map  = [ordered]@{
+            "key1" = @{ id = "a" }
+            "key2" = @{ id = "b" }
         }
-        It 'resolves to list of keys' {
+        Keys = @("key1", "key2")
+    }
+    @{
+        Name = "one-level simple list"
+        Map  = [ordered]@{
+            "key1" = @{
+                list = ("a", "b")
+            }
+            "key2" = @{ id = "b" }
+        }
+        Keys = @("key1*", "a", "b", "key2")
+    }
+) {
+    Describe "<name>" {
+        It '<name> resolves to list of keys' {
             $result = Get-CompletionList $map
-            $result.Keys | Should -be @("key1*", "a", "b", "key2")
+            $result.Keys | Should -Be $keys
         }
     }
 }
