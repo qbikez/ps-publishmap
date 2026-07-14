6cc26887947b835d28a7a230fb6ba358539226d3
jakub.pawlowski <jakub.pawlowski@guestline.com>
2026-07-14 07:48:37 +0200

    debug

--- scripts/imports/set-moduleversion.ps1
+++ scripts/imports/set-moduleversion.ps1
@@ -1,48 +1,49 @@
 function Set-ModuleVersion(
-    [parameter(mandatory=$true)]
+    [parameter(mandatory = $true)]
     $path,
-    [parameter(mandatory=$true)]
+    [parameter(mandatory = $true)]
     [string]$version
-    ) 
-{
+) {
     if ($path.EndsWith(".psd1")) {
         $psd = $path
     }
     elseif ($path.EndsWith(".psm1")) {
-        $psd = $path -replace ".psm1",".psd1"
+        $psd = $path -replace ".psm1", ".psd1"
     }
     else {
-        $modulename = split-path -leaf $path
+        $modulename = Split-Path -Leaf $path
         $psd = "$path\$modulename.psd1"
     }
-    if (!(test-path $psd)) {
+    if (!(Test-Path $psd)) {
         throw "psd1 file '$psd' not found"
     }
 
     $c = Get-Content $psd | Out-String 
-    if ($c -match "ModuleVersion\s=\s'(.+)'") {
-        write-host "replacing version $($Matches[1]) with $version in $psd"
+    if ($c -match "ModuleVersion\s*=\s*'(.+)'") {
+        Write-Host "replacing version $($Matches[1]) with $version in $psd"
     }
-    $c = $c -replace "ModuleVersion\s=\s'.+'","ModuleVersion = '$version'" 
-    $c | Out-File $psd -encoding utf8
+    else {
+        throw "ModuleVersion not found in $psd"
+    }
+    $c = $c -replace "ModuleVersion\s*=\s*'.+'", "ModuleVersion = '$version'" 
+    $c | Out-File $psd -Encoding utf8
 }
 
 function Get-ModuleVersion(
-    [parameter(mandatory=$true)]
+    [parameter(mandatory = $true)]
     $path
-    ) 
-{
+) {
     if ($path.EndsWith(".psd1")) {
         $psd = $path
     }
     elseif ($path.EndsWith(".psm1")) {
-        $psd = $path -replace ".psm1",".psd1"
+        $psd = $path -replace ".psm1", ".psd1"
     }
     else {
-        $modulename = split-path -leaf $path
+        $modulename = Split-Path -Leaf $path
         $psd = "$path\$modulename.psd1"
     }
-    if (!(test-path $psd)) {
+    if (!(Test-Path $psd)) {
         throw "psd1 file '$psd' not found"
     }
     $c = Get-Content $psd | Out-String 
