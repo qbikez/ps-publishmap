5fd5ede5e10a2b008d41db6aed30c6a5424b935d
jakub.pawlowski <jakub.pawlowski@live.com>
2026-07-25 06:52:15 +0200

    fix concurrently log

--- src/configmap/src/plugins/plugin-concurrently/concurrently.ps1
+++ src/configmap/src/plugins/plugin-concurrently/concurrently.ps1
@@ -119,7 +119,8 @@ function Invoke-Concurrently {
         $a += $command
     }
 
-    Write-Verbose "[concurrently] Running: npx $($a | % { "'$_'" } -join ' ')"
+    
+    Write-Verbose "[concurrently] Running: npx $(@($a | % { "'$_'" }) -join ' ')"
     & npx @a | out-host
     if ($LASTEXITCODE -ne 0) {
         throw "concurrently exited with code $LASTEXITCODE"
